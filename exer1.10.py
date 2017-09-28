155fe28204671873712d8f8fd0c4aeec6a5fb15c
Jekton Luo <ljtong64@gmail.com>
2017-09-28 17:02:21 +0800

    Exercise 1.10 Plot P from our sample and the Hoeffding bound

--- exer1.10.py
+++ exer1.10.py
@@ -12,15 +12,12 @@ def simulate(ncoin, ntoss):
     heads /= 10
     return heads[0], np.random.choice(heads), np.min(heads)
 
-def hoeffding(mu, nu, N):
-    epsilon = np.abs(mu - nu)
-    return 2 * np.exp(-2 * epsilon * epsilon * N)
 
 
 ncoin = 1000
 ntoss = 10
 mu = 0.5
-nexperiment = 100
+nexperiment = 10000
 
 experiments = np.empty((nexperiment, 3))
 for i in range(nexperiment):
@@ -35,4 +32,25 @@ plt.hist(experiments[:, 2], bins, alpha=0.5, label="minimun")
 plt.legend()
 plt.show()
 
+
+def P(outcomes, mu, epsilon):
+    less = np.sum(outcomes < mu - epsilon)
+    greater = np.sum(outcomes > mu + epsilon)
+    return (less + greater) / len(outcomes)
+
+def hoeffding(epsilon, N):
+    return 2 * np.exp(-2 * epsilon * epsilon * N)
+
+epsilons = np.linspace(0, 1, 100)
+P_first = [P(experiments[:, 0], mu, epsilon) for epsilon in epsilons]
+P_random = [P(experiments[:, 1], mu, epsilon) for epsilon in epsilons]
+P_min = [P(experiments[:, 2], mu, epsilon) for epsilon in epsilons]
+P_hoeffding = [hoeffding(epsilon, ntoss) for epsilon in epsilons]
+
+plt.plot(epsilons, P_first, label="first")
+plt.plot(epsilons, P_random, label="random")
+plt.plot(epsilons, P_min, label="min")
+plt.plot(epsilons, P_hoeffding, label="Hoeffding")
+plt.legend()
+plt.show()
 
